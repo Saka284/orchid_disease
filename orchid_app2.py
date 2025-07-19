d7331aeb27d44fba15861fc96c3b51eb2c5099aa
saka <saka31.sw@gmail.com>
2025-07-19 16:21:03 +0700

    change weight

--- orchid_app2.py
+++ orchid_app2.py
@@ -430,7 +430,7 @@ DISEASE_CLASSES = ["Petal Blight", "Brown Spot", "Soft Rot"]
 def load_model():
     """Load YOLO model from .pt file"""
     try:
-        model = YOLO("yolobest.pt")
+        model = YOLO("best.pt")
         return model
     except Exception as e:
         st.error(f"❌ Error loading model: {str(e)}")
